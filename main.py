b806a20182e6edac3f57272927f3413e94fa08a2
Zivan-R <135060460+Zivan-R@users.noreply.github.com>
2025-04-17 00:08:19 +0200

    forgot return and print...

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
+        return file_contents
         
 def main():
-    get_book_text("books/frankenstein.txt")
+    print(get_book_text("books/frankenstein.txt"))
     
 main()
