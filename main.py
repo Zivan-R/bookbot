7cc5a7caa2d19545c72b4be19a9e2d2280615630
Zivan-R <135060460+Zivan-R@users.noreply.github.com>
2025-04-17 00:12:41 +0200

    added num_words function

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
+
+def num_words(text):
+    text_list = text.split()
+    return len(text_list)
         
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    text = get_book_text("books/frankenstein.txt")
+    print(f"{num_words(text)} words found in the document")
     
 main()
